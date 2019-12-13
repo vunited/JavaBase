9d00b8bb09eec38b661efab467a4ab01a80071e0
vunited <vunited@vunited.net>
2019-12-13 19:57:06 +0800

    更新12-13

---
+++ Junit/com/junit_1/Class_1_1.java
@@ -0,0 +1,28 @@
+package com.junit_1;
+
+public class Class_1_1 {
+
+	/**
+	 * 框架和工具的区别
+	 * 框架提供一个可以复用的公共结构，可以在多个应用程序中进行共享。
+	 * 开发人员将框架融入到他们自己的应用程序中
+	 * 并且加以扩展以满足他们疼的需求
+	 * 
+	 * 简言之：框架提供一致的结构，工具提供一种工具类
+	 * 
+	 * 极限编程 www.extremeprogramming.org
+	 * Junit www.junit.org 根据ibm通用公共许可证 common public license 1.0 版本进行发布
+	 * 被称为xUnit相关测试框架
+	 * 最初单元测试术语用来描述一项检查单个工作单元的行为测试。
+	 * IEEE组织已经将单元测试定义为“单个硬件，软件单元或一组相关的单元的测试”
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("o");
+		
+	}
+
+}
