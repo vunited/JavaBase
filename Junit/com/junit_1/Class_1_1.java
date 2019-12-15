03362faf7c6a9937390ef3df22679dd961c7a64c
Administrator <Administrator@SC-201909300624>
2019-12-15 23:13:08 +0800

    12-15

--- Junit/com/junit_1/Class_1_1.java
+++ Junit/com/junit_1/Class_1_1.java
@@ -1,5 +1,7 @@
 package com.junit_1;
 
+import java.nio.channels.IllegalSelectorException;
+
 public class Class_1_1 {
 
 	/**
@@ -18,10 +20,43 @@ public class Class_1_1 {
 	 * 
 	 * @param args
 	 */
+	public static class Calculator{
+		double add(double i,double j){
+			return (i+j);
+		}
+		
+	}
+	
+	public static class CalculatorTest{
+		private int nbErrors = 0;
+		
+		public void testAdd() {
+			Calculator calculator = new Calculator();
+			double result = calculator.add(10,50);
+			//手动造异常
+			if(result == 60 ) {
+				throw new IllegalStateException("错的离谱！"+result);
+			}
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("o");
+		System.out.println("lol");
+		CalculatorTest test = new CalculatorTest();
+		try {
+			test.testAdd();
+		} catch (Throwable e) {
+			// TODO: handle exception
+			test.nbErrors++;
+			e.printStackTrace();
+			
+		}
+		if(test.nbErrors > 0) {
+			throw new IllegalStateException("!!!"+test.nbErrors);
+		}
+		
 		
 	}
 
